0fb34543e4abe50c22d5cf2e7686783f48dab5f2
Jonathan Griffin <jgriffin@mozilla.com>
2012-02-01 10:17:14 -0800

    Add code to cleanup old builds

--- publish_b2g.py
+++ publish_b2g.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import datetime
 import os
 import shutil
 import subprocess
@@ -21,6 +22,16 @@ def publish_build(commit, filename):
     msg.set_data('commit', commit)
     publisher.publish(msg)
 
+def clean_old_builds(outdir):
+    # delete builds in the output dir > 2 days old
+    for afile in os.listdir(outdir):
+        stat = os.stat(os.path.join(outdir, afile))
+        if (datetime.datetime.now() - 
+            datetime.datetime.fromtimestamp(stat.st_mtime) > 
+            datetime.timedelta(days=2)):
+            print 'removing old file', afile
+            os.remove(os.path.join(outdir, afile))
+
 if __name__ == '__main__':
     args = sys.argv[1:]
 
@@ -36,6 +47,8 @@ if __name__ == '__main__':
     indir = cfg.get('build', 'in_dir')
     outfile = cfg.get('build', 'out_filename')
     outdir = cfg.get('build', 'out_dir')
+
+    clean_old_builds(outdir)
 
     proc = subprocess.Popen(['git', 'rev-list', '--max-count=1', 'HEAD'],
                             stderr=subprocess.STDOUT, stdout=subprocess.PIPE,
